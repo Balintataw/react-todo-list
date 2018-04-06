57e0c1f092e7943a16aa85a3d002f7b8367f1fd5
balintataw <balintataw2@gmail.com>
2018-04-05 17:04:15 -0700

    form roughed in / needs to add to db.json on submit

--- client/src/components/App/ItemsList.js
+++ client/src/components/App/ItemsList.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react'
-import {connect} from 'react-redux'
-import {Dropdown} from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { List, Image, Button } from 'semantic-ui-react'
 import { getListItems } from '../../actions/listActions'
 import { addItemToList } from '../../actions/listActions'
 
-// const options = [
-//     { key: 'English', text: 'English', value: 'English' },
-//     { key: 'French', text: 'French', value: 'French' },
-//     { key: 'Spanish', text: 'Spanish', value: 'Spanish' },
-//     { key: 'German', text: 'German', value: 'German' },
-//     { key: 'Chinese', text: 'Chinese', value: 'Chinese' },
-// ]
+import './itemsList.css'
 
 export class ItemsList extends Component {
     state = { 
@@ -18,49 +12,49 @@ export class ItemsList extends Component {
     }
 
     static defaultProps = {
-        options:[
-            // {
-            //     key: '',
-            //     text: ''
-            // }    
-        ],
+        options:[ ],
     }
     componentDidMount = () => {
         getListItems()
     }
 
     handleAddition = (e, { value }) => {
-        // this.setState({
-        //     options: [{ message: value }, ...this.props.options],
-        // })
         console.log(value)
         addItemToList(value)
     }
             
-    handleChange = (e, { value }) => {
+    handleChange = ({ target }) => {
         this.setState({ 
-            currentValue: value 
+            [target.name]: target.value 
         })
     }        
     render() {
-        const { currentValue } = this.props
-    
         return (
-            <div>
-                <div>{console.log(this.props)}
-            <Dropdown
-            options={this.props.options}
-            placeholder='What needs to be done?'
-            search
-            open
-            selection
-            fluid
-            allowAdditions
-            value={currentValue}
-            onAddItem={this.handleAddition}
-            onChange={this.handleChange}
-            />
-            </div>
+            <div className="list-wrapper">
+                <form action="">
+                    <input  type="text"
+                            id="task-input"
+                            placeholder="What needs to be done?" 
+                            name="currentValue"
+                            value={this.state.currentValue}
+                            onChange={this.handleChange}
+                            onSubmit={this.handleAddition}
+                    />
+                    <button type="submit" id="add-btn">Add</button>
+                </form>
+                <List animated divided selection verticalAlign='middle'>
+                    {this.props.options.map((task, i) => {
+                        return  <List.Item key={"task" + i} className="list-item-wrapper">
+                                    <input type="checkbox" className="checkbox"/>
+                                    <List.Content className="message-wrapper">
+                                        <List.Content className="message">{task.text}</List.Content>
+                                    </List.Content>
+                                    <List.Content floated='right'>
+                                        <Button>X</Button>
+                                    </List.Content>
+                                </List.Item>
+                    })}
+                </List>
             </div>
         )
     }
